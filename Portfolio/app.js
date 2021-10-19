ecbe40cd8074bf12a82ef6d8de804c3d7e3c522d
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-10-20 00:31:25 +0530

    Update app.js

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -3,20 +3,13 @@ const aside = document.querySelector("aside");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("change");
-
-  if (menuBtn.classList.contains("change")) {
-    aside.classList.add("show-menu");
-    aside.classList.remove("hide-aside");
-  } else {
-    aside.classList.remove("show-menu");
-    aside.classList.add("hide-aside");
-  }
+  //toggle aside bar
+  aside.classList.toggle("show-menu");
+  aside.classList.toggle("hide-aside");
 });
 
 //reset menu btn and aside bar
 window.addEventListener("scroll", () => {
-  if (menuBtn.classList.contains("change")) {
-    aside.classList.add("hide-aside");
-    menuBtn.classList.toggle("change");
-  }
+  aside.classList.add("hide-aside");
+  menuBtn.classList.remove("change");
 });
